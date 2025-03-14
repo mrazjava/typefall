7224aad67c4e7a0dc57357faa059a23128d59073
mrazjava <mrazjava@protonmail.com>
2025-03-14 17:28:46 +0100

    readme resources

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # typefall
+
+
+# resources
+https://github.com/mabe02/lanterna/blob/master/docs/using-terminal.md
+https://www.youtube.com/watch?v=HwPjeb2xnEo
+https://www.youtube.com/watch?v=kT4JYQi9w4w&t=2s
+https://dzone.com/articles/how-to-build-a-command-line-text-editor-with-java
+https://github.com/kba/jcurses?tab=readme-ov-file#installation
+https://github.com/marcobehlerjetbrains/text-editor/tree/episode-1
+https://github.com/nyholku/purejavacomm/blob/master/src/jtermios/JTermios.java
--- src/main/java/com/mrazjava/typefall/Main.java
+++ src/main/java/com/mrazjava/typefall/Main.java
@@ -8,6 +8,31 @@ public class Main {
         Injector injector = Guice.createInjector(new TypefallModule());
         Game game = injector.getInstance(Game.class);
         game.run();
+//    	// Setup terminal and screen layers
+//        Terminal terminal = new DefaultTerminalFactory().createTerminal();
+//        Screen screen = new TerminalScreen(terminal);
+//        screen.startScreen();
+//
+//        // Create panel to hold components
+//        Panel panel = new Panel();
+//        panel.setLayoutManager(new GridLayout(2));
+//
+//        panel.addComponent(new Label("Forename"));
+//        panel.addComponent(new TextBox());
+//
+//        panel.addComponent(new Label("Surname"));
+//        panel.addComponent(new TextBox());
+//
+//        panel.addComponent(new EmptySpace(new TerminalSize(0,0))); // Empty space underneath labels
+//        panel.addComponent(new Button("Submit"));
+//
+//        // Create window to hold the panel
+//        BasicWindow window = new BasicWindow();
+//        window.setComponent(panel);
+//
+//        // Create gui and start gui
+//        MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(), new EmptySpace(TextColor.ANSI.BLUE));
+//        gui.addWindowAndWait(window);
     }
 }
 
