39de5f4c5723c84efab719e747ba2fc8c771dd06
mrazjava <mrazjava@protonmail.com>
2025-03-19 17:31:32 +0100

    init commit: spring boot with jni

---
+++ src/main/java/com/mrazjava/typefall/TypefallApplication.java
@@ -0,0 +1,13 @@
+package com.mrazjava.typefall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypefallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypefallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mrazjava/typefall/TypefallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrazjava.typefall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TypefallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
